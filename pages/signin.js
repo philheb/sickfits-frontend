3e923fad6ee10eccdbe896fe9b99ef02a5c151fa
Philippe Hébert <philippe.heb@gmail.com>
2021-11-03 19:37:55 +0100

    s

--- pages/signin.js
+++ pages/signin.js
@@ -1,6 +1,6 @@
+import styled from 'styled-components';
 import SignIn from '../components/SignIn';
 import SignUp from '../components/SignUp';
-import styled from 'styled-components';
 import RequestReset from '../components/RequestReset';
 
 const GridStyles = styled.div`
